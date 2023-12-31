89b59132fbcfceae83750095e4aecfcbfc2dd342
onur yanık <ya_onur@hotmail.com>
2023-12-31 09:02:58 +0300

    ss

--- wasm/greet/src/lib.rs
+++ wasm/greet/src/lib.rs
@@ -2,15 +2,7 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, greet!");
-}
 #[wasm_bindgen]
 pub fn sum(a: i32, b: i32) -> i32 {
     a + b
--- wasm/greet/tests/web.rs
+++ wasm/greet/tests/web.rs
@@ -4,10 +4,12 @@
 
 extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
+use greet::sum;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+
+fn test_sum() {
+    assert_eq!(sum(2, 2), 4);
 }
