e637145fd3ab628b74d85e2b7fe50b36db92c59a
onur yanık <ya_onur@hotmail.com>
2024-02-09 22:09:57 +0300

    ss

--- front/src/routes/[[lang=lang]]/+page.svelte
+++ front/src/routes/[[lang=lang]]/+page.svelte
@@ -18,17 +18,44 @@
 	onMount (async () => {
 	
 	})
+	let width = 500;
+	let maxWidth=600
 	
 </script>
 
 
  
-<div use:melt={$root}>
+<div use:melt={$root} class="@container/main" >
   <button use:melt={$trigger}>{$open ? 'Close' : 'Open'}</button>
   {#if $open}
 	 <!-- content here -->
 	 <div use:melt={$content} transition:slide>Obi-Wan says: Hello there!</div>
   {/if}
+  <p class="@[80rem]/main:underline">{m._test()}</p>
 </div>
-<p>{m._test()}</p>
+<input type="range" bind:value={width} min="0" max="1600" />
+<div  id="map-div" class="bg-slate-400" style="width:{width}px">
+	<p>Width:{width}px</p>
+	<p class="test-class">Testing color</p>
+
+</div>
+
+<style>
+
+	#map-div {
+		container-type: inline-size;
+    }
+	.test-class{
+		--mx-width:600px;
+
+	}
+	
+    /* @container (width< calc(var(--mx-width))) { */
+    @container (width< 600px) {
+        .test-class {
+            color:#1a9850 ;
+        }
+    
+    }
+</style>
 
